8c66e3d949ea228d22c84a50143045074a734034
tennga <tenngawang6080@gmail.com>
2020-05-27 15:21:58 -0400

    api data successful insert into html

--- js/script.js
+++ js/script.js
@@ -1,7 +1,29 @@
 const BASE_URL = "https://restcountries.eu/rest/v2/all";
 const PARAM = "?fields=name;flag;"
 
+const insertHTML = (countriesData) => {
+    const bodyContainer = document.querySelector('.body-container');
+    countriesData[0].map(country => {
+        bodyContainer.insertAdjacentHTML("beforeend", `
+        <img src=${country.flag}>
+    `)
+    })
+}
 
+const handleCountryData = (data) => {
+    let endPoint = 10;
+    let startPoint = 0;
+    const countryPerPage = [];
+    data.length = 50;
+
+    while(endPoint <= 49){
+        countryPerPage.push(data.slice(startPoint,endPoint));
+        startPoint = endPoint;
+        endPoint = endPoint + 10;
+    }
+    
+    insertHTML(countryPerPage);
+}
 
 
 fetch(BASE_URL + PARAM)
