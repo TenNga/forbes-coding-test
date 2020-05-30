5b8bddebbcb53f77c16e245e971e21d00023eac5
tennga <tenngawang6080@gmail.com>
2020-05-30 01:32:57 -0400

    improved js

--- js/modal.js
+++ js/modal.js
@@ -1,19 +1,13 @@
 const modal = document.querySelector(".modal");
 
-const toggleModalDisplay =  {
-    open: ()=>{
-        modal.classList.remove("close")
-        modal.classList.add("open");
-    },
-    close: () => {
-        modal.classList.remove("open")
-        modal.classList.add("close");
-    }
+const toggleModalDisplay =  () => {
+   modal.classList.toggle("open");
 }
+
 //onclick image, display image in modal
 const handleImgClick = (e) => {
     const modalImag = document.querySelector('#modal-img');
-    toggleModalDisplay.open();
+    toggleModalDisplay();
 
     if (modalImag) 
         modalImag.src = e.target.src;
@@ -24,10 +18,7 @@ const handleImgClick = (e) => {
 }
 
 //close modal on clicking outside image
-const handleOuterClickModal = (e) => {
+const closeModal = (e) => {
     if (e.target.id === "modal-img") return;
-    toggleModalDisplay.close();
+    toggleModalDisplay();
 }
-
-// close modal on clicking X
-const handleCloseModal = () => toggleModalDisplay.close();
